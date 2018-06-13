862fcd1d61824a6511fbb052e8f2597958b10c68
Anna <annala1109@gmail.com>
2018-06-13 13:38:12 +0300

    snake work with touch

--- app/src/main/java/com/androidtutorialshub/LuckyWithYou/activities/SnakeGameActivity.java
+++ app/src/main/java/com/androidtutorialshub/LuckyWithYou/activities/SnakeGameActivity.java
@@ -1,6 +1,8 @@
 package com.androidtutorialshub.LuckyWithYou.activities;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
 import com.androidtutorialshub.LuckyWithYou.snake.HealtySnakeGame;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -8,9 +10,23 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 public class SnakeGameActivity extends AndroidApplication{
 
+    private String userEmail;
+    private ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if(extras == null) {
+                userEmail= null;
+            } else {
+                userEmail= extras.getString("EMAIL");
+            }
+        } else {
+            userEmail= (String) savedInstanceState.getSerializable("EMAIL");
+        }
 
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
         initialize(new HealtySnakeGame(), config);
